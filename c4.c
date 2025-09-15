5a443f2a2d3847e3ef2b93d087e754bef31f9425
ShabinaAslah <shabinashan16@gmail.com>
2025-09-15 14:05:38 +0530

    Add files via upload

---
+++ c4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if(n%2==0)
+	{
+		printf("numberv is even");
+	}
+else
+	{
+		printf("not an even number");
+	}
+}
