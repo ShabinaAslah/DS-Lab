d7d7b0a83ed02c2fe17951230c945fd97b608bc7
ShabinaAslah <shabinashan16@gmail.com>
2025-12-02 16:10:23 +0530

    Add files via upload

---
+++ Recordprgm/bst.c
@@ -0,0 +1,160 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+int data;
+struct node *lchild, *rchild;
+};
+struct node *insert(struct node *root,int data)
+{
+struct node *t1,*t2,*t;
+t1=root;
+t2=(struct node *)0;
+while(t1!=(struct node *)0 && t1->data!=data)
+{
+t2=t1;
+if(data<t1->data)
+t1=t1->lchild;
+else
+t1=t1->rchild;
+}
+if(t1!=(struct node *)0)
+printf("duplicate!!");
+else
+{
+t=(struct node *)malloc(sizeof(struct node *));
+t->data=data;
+t->lchild=>rchild=(struct node *)0);
+if(root==(stuct node *)0)
+root=t;
+else
+{
+if(data<t2->data)
+t2->lchild=t;
+else
+t2->rchild=t;
+}printf("%d is insert",data)
+}
+return root;
+}
+void inorder(struct node *root)
+{
+if(root!=(struct node *)0)
+{
+inorder(root->lchild);
+printf("%d\t",root->data);
+inorder(root->lchild);
+}
+}
+struct node *search(struct node *root,int item)
+{
+struct node *t=root;
+while(t!=(struct node *)0&&t->data!=item)
+if(item<t->data)
+t=t->lchild;
+else
+t=t->rchild;
+return t;
+}
+struct node *delete(struct node *root,int item)
+struct node *t1, *t, *par, *sucpar, *suc;
+t=root;
+par=(struct node *)0;
+while(t!=(struct node *)0&&t->data!=item)
+{
+par=t;
+if(item->data)
+t=t->lchild;
+else
+t=t->rchild;
+}
+if(t==struct node *)0)
+printf("%d not found \n",item);
+else
+{
+if(t->lchild==(struct node *)0&&t->rchild==(struct node *)0)
+case
+{
+if(par==(struct node *)0)
+root=(struct node *)0;
+else
+if(t->data->par->data)
+par->rchild=(struct node *)
+else
+par->lchild=(struct node *)
+}
+else if(t->lchild==(struct node *)0 )|| (t->rchild==(struct node *)0)
+{
+if(par==(struct node *)0)
+root=root->lchild==(struct node *)0?t->rchild=:t->lchild;
+else if(t->data->par->data)
+par->richild=t->lchild==(struct node *)0?t->rchild:t->lchild;
+else
+par->richild=t->lchild==(struct node *)0?t->rchild:t->lchild;
+}
+else
+{
+sucpar=t;
+suc=t->rchild;
+while(suc->lchild!=0)
+{
+sucpar=suc;
+suc=suc->lchild;
+}
+t->data=suc->data;
+if(suc->data<sucpar->data)
+sucpar->lchild=suc->rchild;
+else
+sucpar->rchild=suc->lchild;
+t=suc;
+}
+free(t);
+}
+return root;
+}
+int main()
+{
+struct node *t1,*t=(struct node *)0;
+int data,opt;
+do
+{
+printf("1.insert\n");
+printf("1.insert\n");
+printf("1.insert\n");
+printf("1.insert\n");
+printf("1.insert\n");
+printf("%d",&opt);
+switch(opt)
+{
+case 1:
+				printf("enter data:");
+				scanf("%d",&data);
+				t=insert(t,data);
+				break;
+case 2:inorder(t);
+				break;
+case 3:
+				printf("item to search:")
+				scanf("%d",&data);
+				t1=search(t,data);
+				if(t1==(struct node *)0)
+				{
+				printf("not found");
+				}
+				else
+				{
+				printf("item found");
+				}
+				break;
+case 4:
+				printf("item to delete:");
+				scanf("&d",&item);
+				t=delete(t,data);
+				break;
+case 5:
+				exit(0);'
+}
+}
+while(1);
+}
+				
